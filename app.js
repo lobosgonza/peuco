0c96c42a60157d4ca85b8900a15dcd301b4e0942
unknown <lobosgonza60@gmail.com>
2020-11-09 08:49:10 -0300

    arreglo package.json para quitar firebase

--- app.js
+++ app.js
@@ -119,11 +119,3 @@ module.exports = app;
 // console.log("Server is running on port 3000")
 //   })
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
